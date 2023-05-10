f7e3f98930db75adf887c4a71b0114ff3721e5fd
gabriel <gaby.bunescu@gmail.com>
2023-05-10 22:21:28 +0300

    crearea autor/carte

---
+++ app.py
@@ -0,0 +1,164 @@
+from flask import Flask, render_template, url_for, request, redirect
+from werkzeug.utils import secure_filename
+
+from db import db_init, db
+from model import Carte, Autor
+from io import BytesIO
+from PIL import Image
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///biblioteca.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db_init(app)
+
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/carti')
+def carti():
+    carti = Carte.query.order_by(Carte.date.desc()).all()
+    return render_template("carti/carte.html", carti=carti)
+
+
+@app.route('/carti/<int:id>')
+def carte(id):
+    carte = Carte.query.get_or_404(id)
+    return render_template("creare.html", carte=carte)
+
+
+@app.route('/carti/creare', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        titlu = request.form['titlu']
+        intro = request.form['intro']
+        autor_id = request.form['autor_id']
+
+        # Procesam imaginea din formular
+        image = None
+        if 'image' in request.files:
+            file = request.files['image']
+            if file.filename != '':
+                img = Image.open(BytesIO(file.read()))
+                filename = secure_filename(file.filename)
+                save_path = os.path.join('UPLOAD_FOLDER', filename)  # Calea către mapa 'imagini' și numele fișierului
+                img.save(save_path)
+
+                image = save_path  # Salvăm calea către imagine în loc de datele imaginei
+
+        carte = Carte(titlu=titlu, intro=intro, image=image, autor_id=autor_id)
+
+        try:
+            db.session.add(carte)
+            db.session.commit()
+            return redirect('/carti')
+        except:
+            return "A apărut o eroare la adăugarea cărții."
+    else:
+        autori = Autor.query.all()
+        return render_template("carti/creare.html", autori=autori)
+
+
+
+@app.route('/carti/<int:id>/update', methods=['GET', 'POST'])
+def update(id):
+    carte = Carte.query.get_or_404(id)
+    if request.method == 'POST':
+        carte.titlu = request.form['titlu']
+        carte.intro = request.form['intro']
+        carte.image = request.form['image']
+        carte.autor_id = request.form['autor_id']
+        try:
+            db.session.commit()
+            return redirect('/carti')
+        except:
+            return "A aparut o eroare la actualizarea cartii."
+    else:
+        autori = Autor.query.all()
+        return render_template("update.html", carte=carte, autori=autori)
+
+
+@app.route('/carti/<int:id>/delete', methods=['POST'])
+def delete(id):
+    carte = Carte.query.get_or_404(id)
+    try:
+        db.session.delete(carte)
+        db.session.commit()
+        return redirect('/carti')
+    except:
+        return "A aparut o eroare la stergerea cartii."
+
+
+@app.route('/autori')
+def autori():
+    autori = Autor.query.order_by(Autor.nume, Autor.prenume).all()
+    return render_template("autor/autori.html", autori=autori)
+
+
+@app.route('/autor')
+def autorii():
+    autori = Autor.query.order_by(Autor.nume, Autor.prenume).all()
+    return render_template("autor/autor.html", autori=autori)
+
+@app.route('/autori/<int:id>')
+def autor(id):
+    autor = Autor.query.get_or_404(id)
+    return render_template("autor/detalii.html", autor=autor)
+
+
+@app.route('/autori/create', methods=['GET', 'POST'])
+def create_autor():
+    if request.method == 'POST':
+        nume = request.form['nume']
+        prenume = request.form['prenume']
+        autor = Autor(nume=nume, prenume=prenume)
+        try:
+            db.session.add(autor)
+            db.session.commit()
+            return redirect('/autori')
+        except:
+            return "A aparut o eroare la adaugarea autorului."
+    else:
+        return render_template("autor/create_autori.html")
+
+
+@app.route('/autori/<int:id>/update', methods=['GET', 'POST'])
+def update_autor(id):
+    autor = Autor.query.get_or_404(id)
+    if request.method == 'POST':
+        autor.nume = request.form['nume']
+        autor.prenume = request.form['prenume']
+        try:
+            db.session.commit()
+            return redirect('/autori')
+        except:
+            return "A aparut o eroare la actualizarea autorului."
+    else:
+        return render_template("autor/update_autor.html", autor=autor)
+
+
+@app.route('/autori/<int:id>/delete', methods=['POST'])
+def delete_autor(id):
+    autor = Autor.query.get_or_404(id)
+    try:
+        db.session.delete(autor)
+        db.session.commit()
+        return redirect('/autori')
+    except:
+        return "A aparut o eroare la stergerea autorului."
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
---
+++ model.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from db import db
+
+subs = db.Table('subs',
+                db.Column('carte_id', db.Integer, db.ForeignKey('carte.id')),
+                db.Column('autor_id', db.Integer, db.ForeignKey('autor.id'))
+                )
+
+
+class Carte(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titlu = db.Column(db.String(300), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    image = db.Column(db.LargeBinary, nullable=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow())
+    autor_id = db.Column(db.Integer, db.ForeignKey('autor.id'))
+    autor = db.relationship('Autor', backref='carti')
+
+    def __repr__(self):
+        return '<Carte %r>' % self.id
+
+
+class Autor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nume = db.Column(db.String(300), nullable=False)
+    prenume = db.Column(db.String(300), nullable=True)
+
+    def __repr__(self):
+        return '<Autor %r>' % self.id
